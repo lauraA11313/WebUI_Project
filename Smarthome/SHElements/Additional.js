bf8a1f742dbb3784426185e9d9134db3fddad204
lauraA11313 <51044938+lauraA11313@users.noreply.github.com>
2020-06-09 17:46:29 +0300

    Error managing for menu

---
+++ Smarthome/SHElements/Additional.js
@@ -0,0 +1,38 @@
+//In case there is a need for menu implementation:
+
+
+/*
+function PrintHeader() {
+    console.log("Your house name is " + ourHouse.getHouseName());
+    console.log("Welcome to the SmartHome managing system application\n " +
+        "Start menu:\n" +
+        " 1 - Add new device\n" +
+        " 2 - Change device settings\n" +
+        " 3 - Delete device");
+}       */
+/*
+Error managing code for selection, in case there's a menu implemented
+function getInput(numOfChoices){
+    var choice = -1;
+    while (choice < 1 || choice>numOfChoices){
+        try {
+            console.log("Enter your choice: ");
+            choice = parseInt(prompt("Enter your choice"));
+        }
+        catch (error){
+            alert("Invalid selection. Please try again.");
+        }
+    }
+    return choice;
+}   */
+/*
+function RunMenu() {
+    houseName = "House1";
+    const ourHouse = new SmartHouse(houseName);
+    console.loh
+    PrintHeader();
+    choice1 = getInput(3);
+}
+
+RunMenu();
+*/
